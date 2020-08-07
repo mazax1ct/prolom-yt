65c6d333e739fa2ec1f91fcaaf0fc4a5b0ec5be4
mazax1ct <vova@giveandget.ru>
2020-08-07 16:48:29 +0300

    prolom

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+});
+
+$(document).on('click', '.js-menu-open', function () {
+  $('body').addClass('menu-open');
+  $('.header__menu-block').addClass('is-open');
+  return false;
+});
+
+$(document).on('click', '.js-menu-close', function () {
+  $('body').removeClass('menu-open');
+  $('.header__menu-block').removeClass('is-open');
+  return false;
+});
