4f1c0c5f425206e857232204602f9d0331fb827d
mazax1ct <vova@giveandget.ru>
2020-08-10 19:31:26 +0300

    new blocks

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,9 @@ $(document).on('click', '.js-menu-close', function () {
   $('.header__menu-block').removeClass('is-open');
   return false;
 });
+
+$(document).on('click', '.js-question', function () {
+  $(this).toggleClass('is-active');
+  $(this).next('.answer').slideToggle();
+  return false;
+});
