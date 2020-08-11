d112d1b0c20ddd5bec7c04c6d0e42a52d32f11a0
mazax1ct <vova@giveandget.ru>
2020-08-11 13:31:24 +0300

    end

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,21 @@ $(document).on('click', '.js-menu-close', function () {
   return false;
 });
 
+$(document).on('click', '.main-menu__link', function () {
+  $('body').removeClass('menu-open');
+  $('.header__menu-block').removeClass('is-open');
+
+  $('.main-menu__link').removeClass('is-active');
+  $(this).addClass('is-active');
+
+  var offset = $($(this).attr('href')).offset().top;
+  var header = $('.header').height();
+  var offsetTop = offset - header;
+
+  $('html, body').animate({scrollTop:offsetTop}, '1000');
+  return false;
+});
+
 $(document).on('click', '.js-question', function () {
   $(this).toggleClass('is-active');
   $(this).next('.answer').slideToggle();
